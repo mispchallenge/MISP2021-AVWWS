9b5792f3c229737588d9a9681920cd76e8b75969
mispchallenge <88126124+mispchallenge@users.noreply.github.com>
2022-04-07 14:13:54 +0800

    Update create_train_scp.py

--- kws_net_only_video/create_train_scp.py
+++ kws_net_only_video/create_train_scp.py
@@ -8,42 +8,52 @@ def find_npy(data_root, scp_dir, scp_name='positive'):
     all_wav_paths = []
     for i in data_root:
         all_wav_paths += glob.glob(i)
-    sorted_wav_paths = sorted(all_wav_paths)
-    print(len(sorted_wav_paths))
+    
     lines = ['' for _ in range(1)]
-    for wav_idx in range(len(sorted_wav_paths)):
-        line = sorted_wav_paths[wav_idx]
-        if 'negative_train' in scp_name in scp_name:
-            data = np.load(line)
-            if data.shape[0] > 18:
-                if len(data) > 75:
-                    data = data[(len(data)-75)//2:(len(data)+75)//2]
-                    line = line.replace('.npy', '_limit3s.npy')
-                    np.save(line, data)
+    for wav_idx in range(len(all_wav_paths)):
+        line = all_wav_paths[wav_idx]
         line += '\n'
         lines[0] += line
-
-
+ 
     if not os.path.exists(scp_dir):
         os.makedirs(scp_dir, exist_ok=True)
 
     with codecs.open(os.path.join(scp_dir, '{}.scp'.format(scp_name)), 'w') as handle:
         handle.write(lines[0][:-1])
+
     return None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_root", default='../../MISP2021_AVWWS/', type=str)
+    parser.add_argument("--data_root", default='../get_lip_feature/MISP2021_AVWWS/', type=str)
     args = parser.parse_args()
-    scp_name = ['positive_train', 'negative_train', 'positive_dev', 'negative_dev']
+
     data_root = args.data_root
-    positive_train = [data_root+'positive/video/train/middle/*.npy']
-    negative_train = [data_root+'negative/video/train/middle/*.npy']
-    positive_dev = [data_root+'positive/video/dev/middle/*[0-9].npy']
-    negative_dev = [data_root+'negative/video/dev/middle/*[0-9].npy']
-    find_npy(positive_train, 'scp_dir', 'positive_train')
-    find_npy(negative_train, 'scp_dir', 'negative_train')
-    find_npy(positive_dev, 'scp_dir', 'positive_dev')
-    find_npy(negative_dev, 'scp_dir', 'negative_dev')
-    print('*************')
-    print('*************')
+    
+    # prepare middle field file
+    positive_train_middle = [data_root+'positive/video/train/middle/*.npy']
+    negative_train_middle = [data_root+'negative/video/train/middle/*.npy']
+    positive_dev_middle = [data_root+'positive/video/dev/middle/*[0-9].npy']
+    negative_dev_middle = [data_root+'negative/video/dev/middle/*[0-9].npy']
+    positive_eval_middle = [data_root+'positive/video/eval/middle/*[0-9].npy']
+    negative_eval_middle = [data_root+'negative/video/eval/middle/*[0-9].npy']
+    find_npy(positive_train_middle, 'scp_dir', 'positive_train_mid')
+    find_npy(negative_train_middle, 'scp_dir', 'negative_train_mid')
+    find_npy(positive_dev_middle, 'scp_dir', 'positive_dev_mid')
+    find_npy(negative_dev_middle, 'scp_dir', 'negative_dev_mid')
+    find_npy(positive_eval_middle, 'scp_dir', 'positive_eval_mid')
+    find_npy(negative_eval_middle, 'scp_dir', 'negative_eval_mid')
+
+    # # prepare far field file
+    positive_train_far = [data_root+'positive/video/train/far/*.npy']
+    negative_train_far = [data_root+'negative/video/train/far/*.npy']
+    positive_dev_far = [data_root+'positive/video/dev/far/*[0-9].npy']
+    negative_dev_far = [data_root+'negative/video/dev/far/*[0-9].npy']
+    positive_eval_far = [data_root+'positive/video/eval/far/*[0-9].npy']
+    negative_eval_far = [data_root+'negative/video/eval/far/*[0-9].npy']
+    find_npy(positive_train_far, 'scp_dir', 'positive_train_far')
+    find_npy(negative_train_far, 'scp_dir', 'negative_train_far')
+    find_npy(positive_dev_far, 'scp_dir', 'positive_dev_far')
+    find_npy(negative_dev_far, 'scp_dir', 'negative_dev_far')
+    find_npy(positive_eval_far, 'scp_dir', 'positive_eval_far')
+    find_npy(negative_eval_far, 'scp_dir', 'negative_eval_far')
