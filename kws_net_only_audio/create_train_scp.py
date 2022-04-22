26ae9e43e13fe987e7124e79e0862129db5018d0
xiaozou-gz <65757558+xiaozou-gz@users.noreply.github.com>
2022-04-22 15:09:25 +0800

    Update create_train_scp.py

--- kws_net_only_audio/create_train_scp.py
+++ kws_net_only_audio/create_train_scp.py
@@ -1,57 +1,33 @@
 import os
-import glob
-import codecs
 import argparse
-import scipy.io.wavfile as wf
 
-def find_wav(data_root, scp_dir, scp_name='wpe'):
-    all_wav_paths = []
-    for i in data_root:
-        all_wav_paths += glob.glob(i)
-    sorted_wav_paths = sorted(all_wav_paths)
-    lines = ['' for _ in range(1)]
-    for wav_idx in range(len(sorted_wav_paths)):
-        line = sorted_wav_paths[wav_idx]
-        if 'negative_train' in scp_name:
-            fs, data = wf.read(line)
-            if len(data) > 12000:
-                if len(data) > 48001:
-                    data = data[(len(data)-48000)//2:(len(data)+48000)//2]
-                    line = line.replace('.wav', '_limit3s.wav')
-                    wf.write(line, fs, data)
-        line += '\n'
-        lines[0] += line
 
-    if not os.path.exists(scp_dir):
-        os.makedirs(scp_dir, exist_ok=True)
+def create_train_scp(root_path,data_path,mod):
+    f = open(os.path.join(root_path,'{}_train.scp'.format(mod)),'w')   
+    
+    root_path = os.path.join(root_path,'Beamforming')
+    for root, dirs, files in os.walk(root_path):
+        if mod in root:
+            files.sort()
+            for file in files:
+                f.write(os.path.join(root,file) + '\n')
 
-    with codecs.open(os.path.join(scp_dir, '{}.scp'.format(scp_name)), 'w') as handle:
-        handle.write(lines[0][:-1])
-    return None
+    for root, dirs, files in os.walk(data_path):
+        if mod in root and 'far' not in root and 'train' in root and 'audio' in root:
+            files.sort()
+            for file in files:
+                f.write(os.path.join(root,file) + '\n')
+
+    f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_root", default='./data/', type=str)
-    args = parser.parse_args()  
-    scp_name = ['positive_train', 'negative_train', 'positive_dev', 'negative_dev']
-    data_root = args.data_root
-    SNR_list = [-15,-10,-5,0,5,10,15]
-    positive_train = [data_root+'positive/audio/train/*/*_1_*_*.wav']
-    negative_train = [data_root+'negative/audio/train/*/*_1_*_*.wav']
-    for snr in SNR_list:
-        positive_train.append(data_root+'positive_addnoise/SNR_{}db/*/*/*/*_1mic.wav'.format(str(snr)))
-        negative_train.append(data_root+'negative_addnoise/SNR_{}db/*/*/*/*_1mic.wav'.format(str(snr)))
-    positive_dev = [data_root+'positive/audio/dev/*/*_1_*_*.wav']
-    negative_dev = [data_root+'negative/audio/dev/*/*_1_*_*.wav']
-    positive_dev_middle = [data_root+'positive/audio/dev/middle/*_1_*_*.wav']
-    positive_dev_far = [data_root+'positive/audio/dev/far/*_1_*_*.wav']
-    negative_dev_middle = [data_root+'negative/audio/dev/middle/*_1_*_*.wav']
-    negative_dev_far = [data_root+'negative/audio/dev/far/*_1_*_*.wav']
-    print('*************')
-    find_wav(positive_train, 'scp_dir', 'positive_train')
-    find_wav(negative_train, 'scp_dir', 'negative_train')
-    find_wav(positive_dev_middle, 'scp_dir', 'positive_dev_middle')
-    find_wav(positive_dev_far, 'scp_dir', 'positive_dev_far')
-    find_wav(negative_dev_middle, 'scp_dir', 'negative_dev_middle')
-    find_wav(negative_dev_far, 'scp_dir', 'negative_dev_far')
-    print('*************')
+    parser.add_argument('--root_path', type=str, default='/yrfs1/intern/gzzou2/MISP_TEST', help='root path')
+    parser.add_argument("--data_root", default='/yrfs1/intern/gzzou2/hszhou2_dataset_2022/MISP2021_AVWWS', type=str, help="scp dir")
+    args = parser.parse_args()
+    root_path = args.root_path
+    data_path = args.data_root
+    
+    create_train_scp(root_path,data_path,'positive')
+    create_train_scp(root_path,data_path,'negative')
+    print('Finish!')
