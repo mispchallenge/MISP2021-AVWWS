2a5200fd32a44a84966e9a0de9c6f63d484fccb3
xiaozou-gz <65757558+xiaozou-gz@users.noreply.github.com>
2022-04-22 15:42:36 +0800

    Update run.sh

--- kws_net_only_audio/run.sh
+++ kws_net_only_audio/run.sh
@@ -1,32 +1,17 @@
-#!/usr/bin/env bash
-#
-# This recipe is for misp2021 task1 key word spotting, it judge
-# whether keyword is in the given evaluation utterance or not
-# 
-# Copyright  2021  USTC (Author: Hengshun Zhou, Zhaoxu Nian)
-# Apache 2.0
-
 set -e
 
-python_path=  # e.g. ./python/bin/
-data_root=  # e.g. ./misp2021/
-project=audio_model
-logdir=log
-# collect train and dev corpus
-echo "collect train and dev corpus"
-${python_path}python create_train_scp.py --data_root $data_root
+python_path=""  # e.g. ./python/bin/
+root_path=""  # e.g.  save data dir
+project="save_audio_model"
+logdir="log"
 
-# calculate mean and var of training set
-echo "calculate mean and var of training set"
-if [ ! -e "scp_dir/train_mean_var_fb40_.npz" ]; then
-  ${python_path}python get_mean_var_audio.py
+${python_path}python create_train_scp.py --root_path $root_path --data_root $data_root
+ 
+if [ ! -e "${root_path}/train_mean_var_fb40_.npz" ]; then
+  ${python_path}python get_mean_var_audio.py --root_path $root_path
 fi
 
-# start training procedure
-echo "start training procedure"
 mkdir -p $project
 mkdir -p $logdir
-${python_path}python train.py --end_iter 3 --project $project --logdir $logdir
-
-echo "decode"
-${python_path}python decode.py --load_model ./trained_model/KWS_Lite_Net_1ch_0.model  # choose a model to decode
+${python_path}python -m torch.distributed.launch --nproc_per_node=1 --master_port=12335 train.py --project $project --logdir $logdir
+${python_path}python decode.py --project $project --trained_model_num 10
